9f3f2e8e4d3eefa14434ca2b2f3cede2090ae2c6
Scott Marks <scott@magnolia-heights.com>
2022-04-12 12:27:15 -0400

    code

---
+++ oui.cpp
@@ -0,0 +1,29 @@
+//
+//  oui.cpp
+//  SED
+//
+//  IEEE Organizationally unique identifiers
+//  see https://en.wikipedia.org/wiki/Organizationally_unique_identifier
+//
+//  Created by Scott Marks on 09/10/2017.
+//
+//
+
+#include "oui.hpp"
+typedef struct _data_pair{const char *hex_of_oui; const char *manufacturer;} data_pair;
+static data_pair data[]={
+#define __hex__(oui) #oui
+#include "oui.inc"
+#undef __hex__
+};
+
+static
+std::unordered_map<std::string,std::string>make_table() {
+    std::unordered_map<std::string,std::string>t;
+    for (data_pair *p=&data[0], *pend=&data[sizeof(data)/sizeof(data[0])]; p!=pend; p++)
+        t[p->hex_of_oui]=p->manufacturer;
+    t.reserve( t.size() );
+    return t;
+}
+
+const std::unordered_map<std::string,std::string>manufacturer_for_oui=make_table();
---
+++ oui.hpp
@@ -0,0 +1,20 @@
+//
+//  oui.hpp
+//
+//  SED
+//  IEEE Organizationally unique identifiers
+//  see https://en.wikipedia.org/wiki/Organizationally_unique_identifier
+//
+//  Created by Scott Marks on 09/10/2017.
+//
+//
+
+#ifndef oui_hpp
+#define oui_hpp
+
+#include <string>
+#include <unordered_map>
+
+extern const std::unordered_map<std::string,std::string>manufacturer_for_oui;
+
+#endif /* oui_hpp */
