6ed3d16f8684c7373cba53cb162896611d5d9dc4
Scott Marks <scott@magnolia-heights.com>
2022-11-29 02:15:13 -0500

    cleanup

--- oui.h
+++ oui.h
@@ -16,3 +16,6 @@ FOUNDATION_EXPORT const unsigned char ouiVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <oui/PublicHeader.h>
 
 
+#import <oui/oui.hpp>
+#import <oui/CStringMap.hpp>
+#import <oui/t10_vendorid.hpp>
